ce7cfb67fc54a8f3038e5873cab77cc25049b67c
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-06-06 23:49:58 +0500

    work

---
+++ js/main.js
@@ -0,0 +1,157 @@
+// const navbar = document.querySelector(".hero__collection")
+// 
+// function toggleShow (){
+    // navbar.classList.toggle("show")
+// }
+
+
+
+
+{
+    // //masala 1
+    // let a = +prompt("a")
+    // if (a > 0) {
+    //     a++
+    //     console.log(a);
+    // }else{
+    //     console.log(a);
+    // }
+}
+
+{
+    // //masala 2
+    // let a = +prompt("a")
+    // if (a > 0) {
+    //     a += 1
+    // }
+    // if (a < 0) {
+    //     a -= 2
+    // }
+    // console.log(a);
+}
+
+{
+    // //masala 3
+    // let a = +prompt("a")
+    // if (a > 0) {
+    //     a += 1
+    // }
+    // if (a < 0) {
+    //     a -= 2
+    // }
+    // if (a==0) {
+    //     a += 10
+    // }
+    // console.log(a);
+}
+
+{
+    //masala 4
+    // let x = +prompt("x")
+    // let x1 = +prompt("x1")
+    // let x2 = +prompt("x2")
+    // let musbat = 0
+    // if (x > 0) {
+    //     musbat++
+    // }
+    // if (x1 > 0) {
+    //     musbat++
+    // }
+    // if (x2 > 0) {
+    //     musbat++
+    // }
+    // console.log(musbat);
+}
+{
+    // //masala 5
+    // let x = +prompt("x")
+    // let x1 = +prompt("x1")
+    // let x2 = +prompt("x2")
+    // let musbat = 0
+    // let manfiy = 0
+    // if (x > 0) {
+    //     musbat++
+    // }
+    // if (x < 0) {
+    //     manfiy++
+    // }
+    // if (x1 > 0) {
+    //     musbat++
+    // }
+    // if (x1 < 0) {
+    //     manfiy++
+    // }
+    // if (x2 > 0) {
+    //     musbat++
+    // }
+    // if (x2 < 0) {
+    //     manfiy++
+    // }
+    // console.log(musbat);
+    // console.log(manfiy);
+}
+
+{
+    // //masala 6
+    // let a = +prompt("a")
+    // let b = +prompt("b")
+    // if (a > b) {
+    //    alert(`a (${a}) soni katta `);
+    // }else if (a < b) {
+    //     alert(`b (${b} ) soni katta `)
+    // }
+}
+
+{
+    // //masala 7
+    // let a = +prompt("a")
+    // let b = +prompt("b")
+    // if (a > b) {
+    //    alert(`b (${b}) soni kichik `);
+    // }else if (a < b) {
+    //     alert(`a (${a} ) soni kichik `)
+    // }
+
+}
+
+{
+    // //masala 8
+    // let a = +prompt("a")
+    // let b = +prompt("b")
+    // if (a > b) {
+    //    alert(`a (${a}) soni katta `);
+    // }else if (a < b) {
+    //     alert(`b (${b}) soni katta `);
+    // }
+    // if (a < b) {
+    //     alert(`a (${a} ) soni kichik `)
+    // }else if (a > b) {
+    //     alert(`b (${b} ) soni kichik `)
+    // }
+
+
+}
+
+{
+    // //masala 9
+    // alert("a va b haqiqiy sonlarini kiritib shunaqa o'zgartirish kerakki a soni b sonidan kichik holatda a va b sonlari ekranga chiqsin ")
+    // let a = +prompt("a sonini kiriting")
+    // let b = +prompt("b sonini kiriting")
+    // if (a < b) {
+    //     alert(`${a} soni ${b} sonidan kichik`)
+    // }
+}
+
+{
+    // //masala 10
+    // alert("A  va B butun sonlar berilgan agar ozgaruvchi ozaro teng bolmasa  A va B ozgaruvchilari yigindisi ozlashtirilsin agar teng bolsa 0 ni ozlashtirsin")
+    // let a = +prompt("son kiriting")
+    // let b = +prompt("son kiriting")
+    // if( a != b){
+        // let sum = a + b
+        //  alert(`a soni = ${a} va b soni ${b}
+        //  shular yigindisi ${sum} ga teng`)
+    // }else{
+        // alert(`javob ${0}`)
+    // }
+}
